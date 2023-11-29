240013f4bfcb3d3995e8b7f18ab3de1a412c163d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-29 10:00:25 +0200

    posts

---
+++ js/posts.js
@@ -0,0 +1,13 @@
+'use strict';
+console.log('posts.js file was loaded');
+
+// taikomes
+
+const posts = [
+  { title: 'Post One', body: 'This is post One body' },
+  { title: 'Post Two', body: 'This is post Two body' },
+];
+
+function printPosts() {
+  // sugeneruoti li elmenentus su antraste title, ir textu body
+}
