4947c274d49ab3cb2782489467ae1edd26dd99e5
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-29 11:56:14 +0200

    obj spread

--- js/destruct.js
+++ js/destruct.js
@@ -44,3 +44,26 @@ const { name, town, age } = personObj;
 
 let sentObj = `Mr ${name} is from ${town} and is ${age} year old`;
 console.log('sentObj ===', sentObj);
+
+// spread
+
+const nums = [1, 2, 3];
+const nums2 = [4, 4, 6];
+// const max = Math.max(1, 2, 3);
+const max = Math.max(...nums);
+console.log('max ===', max);
+
+const sujungtasArr = [0, ...nums, 10, ...nums2, 55];
+console.log('sujungtasArr ===', sujungtasArr);
+const nums2copy = [...nums2];
+
+let p5 = {
+  name: 'Mike',
+  town: 'Kaunas',
+  age: 25,
+};
+// sukurti nauja objekta su visom p5 reiksmem plius hasCar reiksme
+
+let p5Copy = { ...p5, hasCar: true };
+// p5Copy.hasCar = true;
+console.log('p5Copy ===', p5Copy);
